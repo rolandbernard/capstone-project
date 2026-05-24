471e1c1bb503d2ecb84653af8417eef9d9e91db5
Roland Bernard <rolli.bernard@gmail.com>
2026-05-24 17:51:56 +0200

    Added covariance computation into the pose detector.

--- code/detect.py
+++ code/detect.py
@@ -3,27 +3,38 @@ import torch
 from ultralytics import YOLO
 from ultralytics.nn.tasks import PoseModel
 
+from camera import Camera
+
 
 class PoseDetector:
     """
     This is an wrapper class around the YOLO based pose estimation models. Given
-    an image it produces as output a tensor of shape (P, K, 3) where P is the
-    number of detected persons an K = 17 is the number of keypoints.
+    an image it produces as output a tensor of shape (P, K*3) where P is the
+    number of detected persons an K = 17 is the number of keypoints. It also
+    gives an estimated covariance matrix based on keypoint visibility.
     """
 
-    def __init__(self, model_name: str = "yolo26n-pose", threshold: float = 0.25, path: str = "./nets"):
+    def __init__(
+        self, model_name: str = "yolo26n-pose", threshold: float = 0.25, min_keypoint: int = 3,
+        var_vis: float = 0.002, var_inv: float = 1.0, path: str = "./nets"
+    ):
         model: PoseModel = YOLO(
             f"{path}/{model_name}.pt").model  # type: ignore
         model.eval()
         model.compile()
         self.model = model
         self.threshold = threshold
+        self.min_keypoint = min_keypoint
+        self.var_vis = var_vis
+        self.var_inv = var_inv
         self.num_keypoint = 17
 
-    def detect(self, images: torch.Tensor | list[torch.Tensor]) -> list[torch.Tensor]:
+    def detect_simple(self, images: torch.Tensor | list[torch.Tensor]) -> list[tuple[torch.Tensor, torch.Tensor]]:
         """
         Run the detection algorithm and return discovered keypoints. We expect
-        a batch of images and return a batch of results.
+        a batch of images and return a batch of results. For the results we return
+        for P persons two tensors of shape (P, 17*2) for the positions of all 17
+        keypoints and and a tensor of shape (P, 17*2, 17*2) for the covariance.
         """
         with torch.inference_mode():
             if isinstance(images, list):
@@ -35,9 +46,32 @@ class PoseDetector:
             pred, _ = self.model(images)
             results = []
             for img_res in pred:
-                valid = img_res[img_res[:, 4] > self.threshold, 6:]
-                results.append(valid.view(-1, self.num_keypoint, 3))
+                valid = img_res[(img_res[:, 4] > self.threshold) &
+                                ((img_res[:, 8::3] > self.threshold).sum() > self.min_keypoint)]
+                valid_points = valid[:, 6:].view(-1, self.num_keypoint, 3)
+                bb_size = torch.linalg.vector_norm(
+                    valid[:, 2:4] - valid[:, 0:2], dim=1, keepdim=True)
+                vis = torch.clamp(
+                    (valid_points[:, :, 2] - self.threshold) / (1.0 - self.threshold), min=0)
+                var = bb_size * bb_size * \
+                    (self.var_vis / (vis + self.var_vis / self.var_inv))
+                results.append((
+                    valid_points[:, :, 0:2].reshape(-1, self.num_keypoint*2),
+                    torch.kron(torch.diag_embed(var),
+                               torch.eye(2, device=var.device))
+                ))
             return results
+
+    def detect(self, cams: list[Camera], images: torch.Tensor | list[torch.Tensor]) -> list[tuple[torch.Tensor, torch.Tensor]]:
+        """
+        This is like `detect_simple`, but the returned keypoints are in normalized
+        camera coordinates rather than in pixel space, effectively removing the
+        camera intrinsics.
+        """
+        return [
+            (cam.undistort_points(pos), cam.undistort_covars(cov))
+            for cam, (pos, cov) in zip(cams, self.detect_simple(images))
+        ]
 
     def to(self, *args, **kargs):
         """
