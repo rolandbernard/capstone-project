5d0a068fd52feb4812ccfe8122a65ab4ab9df895
Roland Bernard <rolli.bernard@gmail.com>
2026-06-23 19:39:00 +0200

    Added back the custom YOLO head training.

--- code/detect.py
+++ code/detect.py
@@ -1,9 +1,18 @@
 
+import os
+
 import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
 from ultralytics import YOLO
 from ultralytics.nn.tasks import PoseModel
+from ultralytics.nn.modules import Detect, Pose26, Conv
+import scipy.optimize
 
+import util
+import dataset
 from camera import Camera
+from util import NetStorage
 
 
 class PoseDetector:
@@ -90,3 +99,270 @@ class PoseDetector:
         """ Apply the PyTorch `.to` method to the contained model. """
         self.model = self.model.to(*args, **kargs)
         return self
+
+
+class CustomPoseDetector(PoseDetector):
+    """
+    This is an wrapper class around the YOLO model with the custom keypoint
+    prediction head. This extends `PoseDetector` even though it does not share
+    nearly any of its functionality.
+    """
+
+    def __init__(self, model: CustomHeadedYolo, threshold: float = 0.5, compile: bool = True):
+        model.eval()
+        if compile:
+            model.compile()
+        self.model = model
+        self.threshold = threshold
+        self.num_keypoint = 17
+
+    def detect_base(self, images: torch.Tensor) -> list[tuple[torch.Tensor, torch.Tensor]]:
+        """
+        Run the detection algorithm and return discovered keypoints.
+        """
+        pred = self.model(images)
+        valid_mask = torch.sigmoid(pred["one2one"]["scores"]) > self.threshold
+        results = []
+        for b in range(images.shape[0]):
+            valid_idx = valid_mask[b].flatten().nonzero(as_tuple=True)[0]
+            kpts = pred["kpts_extra"][b, :, valid_idx] \
+                .view(self.num_keypoint, 5, -1).permute(2, 0, 1)
+            mu = kpts[..., :2]
+            a, b, c = kpts[..., 2], kpts[..., 3], kpts[..., 4]
+            # Compute variance based on cholesky factors.
+            cov = torch.stack([
+                torch.stack([a*a, a*c], dim=-1),
+                torch.stack([a*c, c*c + b*b], dim=-1)
+            ], dim=-2)
+            # Diagonalize the covariances assuming independence.
+            cov = torch.diag_embed(cov.permute(0, 2, 3, 1), dim1=1, dim2=3)
+            results.append((
+                mu.reshape(-1, self.num_keypoint*2),
+                cov.reshape(-1, self.num_keypoint*2, self.num_keypoint*2)
+            ))
+        return results
+
+
+class CustomHeadedYolo(nn.Module):
+    """
+    Small wrapper to install the custom pose head at the end of a standard YOLO26
+    human pose estimation model.
+    """
+
+    def __init__(self, original_model: str | PoseModel = "yolo26n-pose", path: str = "./nets"):
+        super().__init__()
+        # Acquire the base model.
+        if isinstance(original_model, str):
+            original_model = YOLO(
+                f"{path}/{original_model}.pt").model  # type: ignore
+        self.base_net: PoseModel = original_model  # type: ignore
+        # Freeze the base model.
+        self.base_net.eval()
+        for param in self.base_net.parameters():
+            param.requires_grad = False
+        # Create the extra head.
+        self.extra_head = nn.ModuleList(nn.Sequential(
+            Conv(ch, 100, 3),
+            Conv(100, 100, 3),
+            Conv(100, 100, 3),
+            Conv(100, 100, 3),
+            nn.Conv2d(100, 17*5, 1)
+        ) for ch in (64, 128, 256))
+
+    @property
+    def device(self):
+        return next(self.parameters()).device
+
+    @property
+    def anchors(self):
+        return self.base_net.model[-1].anchors
+
+    @property
+    def strides(self):
+        return self.base_net.model[-1].strides
+
+    def train(self, train=True):
+        super().train(train)
+        self.base_net.eval()
+        return self
+
+    def extra_parameters(self):
+        """
+        Get the extra parameters that have been added by the custom head. During
+        training we will be freezing all other parameters.
+        """
+        return self.extra_head.parameters()
+
+    def forward(self, x):
+        bs = x.shape[0]
+        with torch.no_grad():
+            assert not self.base_net.training
+            _, pred = self.base_net(x)
+        extra = torch.cat([
+            self.extra_head[i](features).view(bs, 17*5, -1)
+            for i, features in enumerate(pred["one2one"]["feats"])
+        ], dim=2).view(bs, 17, 5, -1)
+        pred["kpts_extra"] = torch.concat([
+            (extra[:, :, 0:2] + self.anchors) * self.strides,
+            torch.exp(extra[:, :, 2:4]) * self.strides,
+            extra[:, :, 4:5] * self.strides,
+        ], dim=2).view(bs, 17*5, -1)
+        return pred
+
+
+def compute_nll(pred: torch.Tensor, gt: torch.Tensor, w_mse, w_thres=0.05) -> torch.Tensor:
+    """
+    Computes the weighted negative log likelihood loss for 2D Gaussian in the
+    predictions against the ground truth.
+    """
+    mu, a, b, c = pred[..., :2], pred[..., 2], pred[..., 3], pred[..., 4]
+    gt_xy, w = gt[..., :2], gt[..., 2]
+    w = w > w_thres
+    L = torch.stack([
+        torch.stack([a, torch.zeros_like(a)], dim=-1),
+        torch.stack([c, b], dim=-1)
+    ], dim=-2)
+    logdet = 2.0 * (torch.log(a) + torch.log(b))
+    diff = (gt_xy - mu).unsqueeze(-1)
+    v = torch.linalg.solve_triangular(L, diff, upper=False)
+    mahalanobis = v.mT @ v
+    nll = logdet + mahalanobis
+    return torch.mean(nll * w) \
+        + w_mse * torch.mean(torch.sum(diff*diff, dim=(-1, -2)) * w)
+
+
+def compute_loss(pred, gt: torch.Tensor, model, w_mse, threshold: float = 0.0, eps=1e-5):
+    """
+    Compute the loss between the predictions and the ground truth. First, match
+    the detections against the known ground truth and then compute the negative
+    log likelihood over the resulting matches.
+    """
+    bs = gt.shape[0]
+    anchors, strides = model.anchors, model.strides
+    valid_mask = pred["one2one"]["scores"].detach() > threshold
+    all_kpts = pred["one2one"]["kpts"].detach().clone()
+    all_kpts = all_kpts.view(bs, 17, 3, -1)
+    all_kpts[:, :, :2] = (all_kpts[:, :, :2] + anchors) * strides
+    preds = []
+    gts = []
+    for b in range(bs):
+        valid_idx = valid_mask[b].flatten().nonzero(as_tuple=True)[0]
+        if len(valid_idx) == 0:
+            continue
+        b_kpts = all_kpts[b, :, :, valid_idx].permute(2, 0, 1)
+        b_gt = gt[b, torch.sum(gt[b, :, :, 2], dim=-1) > 0.01]
+        dist_matrix = b_kpts[:, :, :2].unsqueeze(1) \
+            - b_gt[:, :, :2].unsqueeze(0)
+        dist_matrix *= dist_matrix
+        weight = torch.sigmoid(b_kpts[:, :, 2]).unsqueeze(1) \
+            * b_gt[:, :, 2].unsqueeze(0)
+        cost_matrix = torch.sum(torch.sum(dist_matrix, dim=-1) * weight, dim=-1) \
+            / (torch.sum(weight, dim=-1) + eps)
+        # 6. Hungarian Matching (Push to CPU only for the solver)
+        cost_np = cost_matrix.cpu().numpy()
+        row_idx, col_idx = scipy.optimize.linear_sum_assignment(cost_np)
+        matched_pred_indices = valid_idx[row_idx]
+        preds.append(
+            pred["kpts_extra"][b, :, matched_pred_indices].view(17, 5, -1).permute(2, 0, 1))
+        gts.append(b_gt[col_idx])
+    if len(preds) == 0:
+        return torch.tensor(0.0, device=gt.device, requires_grad=True)
+    return compute_nll(torch.concat(preds, dim=0), torch.concat(gts, dim=0), w_mse)
+
+
+def train_epoch(model, loader, optimizer, w_mse):
+    """
+    Perform a single training epoch. Also computes the average training loss
+    over the course of the epoch.
+    """
+    # Ensure we are in training mode.
+    model.train()
+    total_loss = 0
+    count = 0
+    for img, gts in loader:
+        img = img.to(model.device, non_blocking=True)
+        gts = gts.to(model.device, non_blocking=True)
+        optimizer.zero_grad(set_to_none=True)
+        pred = model(img)
+        loss = compute_loss(pred, gts, model, w_mse)
+        loss.backward()
+        nn.utils.clip_grad_value_(model.parameters(), clip_value=1.0)
+        optimizer.step()
+        total_loss += loss.item()
+        count += 1
+    return total_loss / count
+
+
+def eval_epoch(model, loader, w_mse):
+    """
+    Run a single evaluation round over the given loader. This is intended to be
+    used after each epoch to evaluate the performance on the validation set.
+    """
+    model.eval()
+    total_loss = 0
+    count = 0
+    # Disable gradients to save memory and compute.
+    with torch.inference_mode():
+        for img, gts in loader:
+            img = img.to(model.device, non_blocking=True)
+            gts = gts.to(model.device, non_blocking=True)
+            pred = model(img)
+            loss = compute_loss(pred, gts, model, w_mse)
+            total_loss += loss.item()
+            count += 1
+    return total_loss / count
+
+
+def train_epochs(nets: NetStorage, train, val, num_epochs: int, w_mse: float, callback=None):
+    """
+    Perform a multiple training epochs, recoding the history of both training
+    and validation loss in the given log directory. This will train using the
+    saved optimizer, model, and learning rate schedule from the provided net
+    storage. Uses pixel-wise cross-entropy as the loss.
+    """
+    # Lower precision to benefit from certain hardware support.
+    torch.set_float32_matmul_precision('high')
+    model = nets.net
+    optimizer = nets.optimizer
+    scheduler = nets.scheduler
+    for epoch in range(nets.step + 1, num_epochs):
+        tr_loss = train_epoch(model, train, optimizer, w_mse)
+        val_loss = eval_epoch(model, val, w_mse)
+        nets.save_network(epoch, {
+            "tr_loss": tr_loss, "val_loss": val_loss,
+        }, model, optimizer, scheduler)
+        scheduler.step(val_loss)
+        print(f"epoch {epoch}; train: loss {tr_loss}; val: loss {val_loss}")
+        if callback is not None and callback(val_loss, epoch):
+            print("stopping via callback")
+            break
+    else:
+        print("max epoch reached")
+
+
+def train_epochs_in(num_epochs: int, nets_dir: str | None, stat_dir: str | None, model, w_mse=0.0, testing=False, callback=None):
+    """
+    Perform a multiple training epochs. This will initialize the net storage in
+    case we are starting a fresh run, and resume the existing run otherwise. The
+    optimizer and learning rate schedule will be initializer according to the
+    passed configuration. The test and train split are generated.
+    """
+    util.set_seed(42)
+    nets = util.net_storage_in(nets_dir, stat_dir, model.to(util.DEVICE))
+    full_train = dataset.YoloDataset(
+        f"{os.path.dirname(__file__)}/data/yolo/train")
+    if testing:
+        # This configuration is only for the sanity check, it is not used for the
+        # actual training of the models.
+        train = torch.utils.data.Subset(full_train, range(32, 128))
+        val = torch.utils.data.Subset(full_train, range(32))
+    else:
+        train = full_train
+        val = dataset.YoloDataset(f"{os.path.dirname(__file__)}/data/yolo/val")
+    train_loader = DataLoader(
+        train, 32, shuffle=True, drop_last=True, num_workers=8,
+        persistent_workers=True, pin_memory=True, prefetch_factor=4)
+    val_loader = DataLoader(
+        val, 32, shuffle=True, drop_last=True, num_workers=8,
+        persistent_workers=True, pin_memory=True, prefetch_factor=4)
+    train_epochs(nets, train_loader, val_loader, num_epochs, w_mse, callback)
--- code/util.py
+++ code/util.py
@@ -4,7 +4,9 @@ import json
 import random
 
 import torch
+import torch.optim as optim
 import numpy as np
+import pandas as pd
 import scipy.optimize
 
 # Slightly lower precision for performance.
@@ -52,6 +54,95 @@ def remove_idx(input: torch.Tensor, idx: torch.Tensor, dim=0) -> torch.Tensor:
     mask = torch.ones(input.shape[dim], dtype=torch.bool, device=input.device)
     mask[idx] = False
     return torch.index_select(input, dim, torch.nonzero(mask).squeeze())
+
+
+class NetStorage:
+    """
+    This class provides some utility methods for recoding training history, as
+    well as saving and restoring model weights. This should allow stopping and
+    resuming the training process after any epoch.
+    """
+
+    @classmethod
+    def training_stats(cls, stats_dir: str) -> pd.DataFrame:
+        return pd.DataFrame([
+            torch.load(f"{stats_dir}/{n}") for n in os.listdir(stats_dir)
+        ])
+
+    def __init__(
+        self, nets_dir: str | None, stat_dir: str | None, net, optimizer, scheduler, compile=True
+    ):
+        self.nets_dir = nets_dir
+        self.stat_dir = stat_dir
+        self.step = -1
+        self.net = net
+        if compile:
+            self.net.compile()
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.load_checkpoint()
+
+    def save_network(self, step: int, metrics: dict, net, optimizer, scheduler):
+        if self.nets_dir is not None:
+            torch.save(net.state_dict(), f"{self.nets_dir}/{step}.net")
+            torch.save(optimizer.state_dict(), f"{self.nets_dir}/{step}.optim")
+            torch.save(scheduler.state_dict(), f"{self.nets_dir}/{step}.sched")
+        if self.stat_dir is not None:
+            torch.save({
+                "step": step,
+                **metrics,
+            }, f"{self.stat_dir}/{step}.stat")
+        # Update the instance variable to reflect the new checkpoint.
+        self.step = step
+        self.net = net
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+
+    def load_best_checkpoint(self):
+        if self.stat_dir is not None:
+            stats = NetStorage.training_stats(self.stat_dir)
+            min_idx = stats["val_loss"].idxmin()
+            self.load_checkpoint(stats.loc[min_idx].step)
+
+    def load_checkpoint(self, step=None):
+        if self.nets_dir is None or self.stat_dir is None:
+            # If any of the directories are missing, recovery is not supported.
+            return
+        # Make sure the directories exist.
+        os.makedirs(self.nets_dir, exist_ok=True)
+        os.makedirs(self.stat_dir, exist_ok=True)
+        # Figure out the latest checkpoints step count.
+        if step is None:
+            step = max((int(n.split(".")[0])
+                       for n in os.listdir(self.stat_dir)), default=-1)
+        # Load if the step exists, otherwise this is a fresh run.
+        if step != -1 and os.path.isfile(f"{self.stat_dir}/{step}.stat"):
+            self.step = step
+            model = torch.load(
+                f"{self.nets_dir}/{step}.net", map_location=self.net.device
+            )
+            self.net.load_state_dict(model)
+            self.net.eval()
+            optim = torch.load(f"{self.nets_dir}/{step}.optim")
+            self.optimizer.load_state_dict(optim)
+            for state in self.optimizer.state.values():
+                for k, v in state.items():
+                    if torch.is_tensor(v):
+                        state[k] = v.to(self.net.device)
+            sched = torch.load(f"{self.nets_dir}/{step}.sched")
+            self.scheduler.load_state_dict(sched)
+            print(f"Loaded checkpoint for step {step}.")
+
+
+def net_storage_in(nets_dir: str | None, stat_dir: str | None, model, compile=True):
+    """
+    Initialize or load the net storage from the specified directories. This
+    function will take the necessary parameters from the supplied configuration.
+    """
+    optimizer = optim.Adam(model.extra_parameters(), lr=3e-3)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode="min", factor=0.5, patience=5, min_lr=1e-6)
+    return NetStorage(nets_dir, stat_dir, model, optimizer, scheduler, compile)
 
 
 def count_model_params(model):
